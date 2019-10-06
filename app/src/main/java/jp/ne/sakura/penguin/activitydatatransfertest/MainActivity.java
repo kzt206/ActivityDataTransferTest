7c3a0d14aca5a972f84fb9613044c303002b95d4
TSUTSUI,Kazuo <kzt206@gmail.com>
2019-10-06 17:53:38 +0900

    int data transfer

--- app/src/main/java/jp/ne/sakura/penguin/activitydatatransfertest/MainActivity.java
+++ app/src/main/java/jp/ne/sakura/penguin/activitydatatransfertest/MainActivity.java
@@ -2,7 +2,10 @@ package jp.ne.sakura.penguin.activitydatatransfertest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                int number = 1000;
+                Intent intent = new Intent(getApplication(),SubActivity.class);
+//                intent.putExtra("Int1",3);
+                intent.putExtra("Int1",number);
+                startActivity(intent);
+
+            }
+        });
     }
 }
